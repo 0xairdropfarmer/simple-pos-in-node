9de9235b7b65f2b0c0b0bea74e4cc77f2ab52dda
krissanawat <krissanawat101@gmail.com>
2018-04-03 06:44:03 +0700

    Update index.js
    Try solve issue #1

--- react-pos/src/index.js
+++ react-pos/src/index.js
@@ -4,7 +4,7 @@ import { BrowserRouter } from "react-router-dom";
 import registerServiceWorker from "./registerServiceWorker";
 import "./index.css";
 import "bootstrap/dist/css/bootstrap.css";
-import { makeRoutes } from "./routes";
+// import { makeRoutes } from "./routes";
 import App from "./App";
 
 render(
